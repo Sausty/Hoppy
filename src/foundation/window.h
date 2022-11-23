ee53a1aac7f23dca1c72c4f40e5464d7ae74cc03
Sausty <MikuoH15TH@gmail.com>
2022-11-23 17:26:19 +0100

    [FIX] Added documentation on window.h

--- src/foundation/window.h
+++ src/foundation/window.h
@@ -25,9 +25,24 @@ namespace hoppy {
         void* platform_handle;
     };
 
+    /// @brief Initialises a window
+    /// @param w A pointer to the window
+    /// @param width The width of the window
+    /// @param height The height of the window
+    /// @param title The title of the window
     void window_init(window *w, uint32_t width, uint32_t height, const char *title);
+    
+    /// @brief Frees the given window
+    /// @param w A pointer to the window to free
     void window_free(window *w);
+    
+    /// @brief Updates the event queue
+    /// @param w A pointer to the window to update
     void window_poll_events(window *w);
+
+    /// @brief Returns whether or not the window is open
+    /// @param w A pointer to the window
+    /// @return True if the window is open; otherwise false
     bool window_is_open(window *w);
 }
 
