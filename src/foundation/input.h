29760364aa81c1172e2d04b520f2d8ec0a954294
Sausty <MikuoH15TH@gmail.com>
2022-11-23 17:31:07 +0100

    [FIX] Added documentation on input.h

--- src/foundation/input.h
+++ src/foundation/input.h
@@ -9,20 +9,35 @@
 #define INPUT_H_
 
 namespace hoppy {
+    /// @brief An enum representing all the buttons of a gamepad
     enum class gamepad_button {
+        /// @brief Up direction on the d-pad
         dpad_up,
+        /// @brief Down direction on the d-pad
         dpad_down,
+        /// @brief Left direction on the d-pad
         dpad_left,
+        /// @brief Right direction on the d-pad
         dpad_right,
+        /// @brief Start button
         start,
+        /// @brief Back button
         back,
+        /// @brief Left thumb button
         left_thumb,
+        /// @brief Right thumb button
         right_thumb,
+        /// @brief Left shoulder button
         left_shoulder,
+        /// @brief Right shoulder button
         right_shoulder,
+        /// @brief The A button
         a,
+        /// @brief The B button
         b,
+        /// @brief The X button
         x,
+        /// @brief The Y button
         y
     };
 
